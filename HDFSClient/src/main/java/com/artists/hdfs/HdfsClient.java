e0bf697d45e5a90a7e5c329d394d9be840824be4
ArtistS-Win <sylgg0918@gmail.com>
2023-02-01 07:17:31 +0800

    fix permission deny problem

--- HDFSClient/src/main/java/com/artists/hdfs/HdfsClient.java
+++ HDFSClient/src/main/java/com/artists/hdfs/HdfsClient.java
@@ -3,6 +3,8 @@ package com.artists.hdfs;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -17,20 +19,27 @@ import java.net.URISyntaxException;
  * HDFS zookeeper
  */
 public class HdfsClient {
-    @Test
-    public void testmkdir() throws URISyntaxException, IOException {
+    private FileSystem fs;
+    @Before
+    public void init() throws IOException, InterruptedException, URISyntaxException {
         // 连接的集群的nn地址
         URI uri = new URI("hdfs://34.132.121.211:8020");
         // 创建一个配置文件
         Configuration configuration = new Configuration();
 
-        // 1.获取客户端对象
-        FileSystem fs = FileSystem.get(uri, configuration);
+        // 用户
+        String user = "sylgg0918";
 
+        // 1.获取客户端对象
+        fs = FileSystem.get(uri, configuration, user);
+    }
+    @After
+    public void close() throws IOException{
+        fs.close();
+    }
+    @Test
+    public void testmkdir() throws URISyntaxException, IOException, InterruptedException {
         // 2.创建一个文件夹
         fs.mkdirs(new Path("/xiyou/huaguoshan"));
-
-        // 3.关闭资源
-        fs.close();
     }
 }
